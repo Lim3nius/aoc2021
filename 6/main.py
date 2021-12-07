7d7fb41a489909b24dce27aa332ad851627950bb
Tomáš Lessy <tomas.lessy@firma.seznam.cz>
2021-12-07 01:38:16 +0100

    Day 6 - blockage

---
+++ 6/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+states = {}
+iter_cnt = 18
+
+
+def simulate_fish(init_s: int, iters: int) -> int:
+    alt_iters = max(0, iters - init_s)
+
+    if r := states.get((alt_iters, 0), None):
+        return r
+
+    childs = []
+    for c in range(0, alt_iters, 7):
+        childs.append((8, alt_iters - c - 1))
+
+    sub_childs = sum([simulate_fish(*c) for c in childs])
+    childs_total = sub_childs + len(childs)
+    states[(alt_iters, 0)] = childs_total
+    return childs_total
+
+
+if __name__ == '__main__':
+    with open('input', 'r') as h:
+        fish_init = list(map(int, h.readline().split(',')))
+
+    acc = 0
+    for (i, f) in enumerate(fish_init):
+        acc += simulate_fish(f, 80) + 1
+    res_fish = acc
+    print(f'Part 1 -> {res_fish}')
+
+    states = {}
+    res = sum([simulate_fish(f, 256)+1 for f in fish_init])
+    print(f'Part 2 -> {res}')
